320a297a512b8ddefb30ba79c9f1816d0c66815a
cobinrox <mescgroup@yahoo.com>
2026-02-17 22:11:22 +0000

    more service fixes

--- stop_service.sh
+++ stop_service.sh
@@ -1,12 +1,2 @@
-#!/bin/bash
-
-# stops if running as a service
-SERVICE_NAME="parrotpi"
-
-echo "Stopping ParrotPi service..."
-sudo systemctl stop $SERVICE_NAME
-
-echo "Disabling ParrotPi service so it won't start on boot..."
-sudo systemctl disable $SERVICE_NAME
-
-echo "ParrotPi service stopped and disabled."
+# stop service, must be run as sudo
+systemctl stop parrotpi
